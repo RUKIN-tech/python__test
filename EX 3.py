be8558aaac6be9fa441fbda1fb33c00e1ce1670e
RUKIN-tech <g.rukin2009@gmail.com>
2025-05-28 16:30:44 +0530

    Add files via upload

---
+++ EX 3.py
@@ -0,0 +1,17 @@
+numbers_x = [10, 20, 30, 40, 10]
+first_number=10
+last_number=10
+if(first_number==last_number):
+    print("true")
+else:
+    print("flase")
+
+
+
+numbers_y = [75, 65, 35, 75, 30]
+first_number=75
+last_number=30
+if(first_number==last_number):
+    print("true")
+else:
+    print("flase")
